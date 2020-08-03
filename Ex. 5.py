7d8bee3da095403558c61099fcb9520a4529533a
Nicolas Florencio Alves <nicolas.alves14@etec.sp.gov.br>
2020-08-03 00:29:34 -0300

    03/08/2020

--- Ex. 5.py
+++ Ex. 5.py
@@ -3,6 +3,12 @@
 	Função: Receber 3 valores e mostrar o quadrado da soma dos números
 	Autor: Nicolas Florencio Alves
 	Data de criação: 31/07/2020
-	Data de modificação: 31/07/2020
+	Data de modificação: 02/08/2020
 -------------------------------------------
 """
+v1 = int(input("Informe o primeiro número: "))
+v2 = int(input("Informe o segundo número: "))
+v3 = int(input("Informe o terceiro número: "))
+print("O quadrado da soma dos 3 valores é: ", (v1+v2+v3)**2)
+
+

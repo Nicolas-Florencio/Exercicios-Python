248f0f964d9bc60ba1bf3a6b5eb3a53df1f8abec
Nicolas Florencio Alves <nicolas.alves14@etec.sp.gov.br>
2021-07-10 16:35:46 -0300

    10/07/2021

---
+++ Ex. 21.py
@@ -0,0 +1,46 @@
+"""
+-----------------------------------------
+	Função: Nome do mês de acordo com o número
+	Autor: Nicolas Florencio Alves.
+	Data de criação: 10/07/2021.
+	Data de modificação: 10/07/2021.
+-------------------------------------------
+"""
+
+numero = int(input("Informe o respectivo número do mês desejado: "))
+
+if numero == 1:
+    print("Janeiro")
+
+elif numero == 2:
+    print("Fevereiro")
+
+elif numero == 3:
+    print("Março")
+
+elif numero == 4:
+    print("Abril")
+
+elif numero == 5:
+    print("Maio")
+
+elif numero == 6:
+    print("Junho")
+
+elif numero == 7:
+    print("Julho")
+
+elif numero == 8:
+    print("Agosto")
+
+elif numero == 9:
+    print("Setembro")
+
+elif numero == 10:
+    print("Outubro")
+
+elif numero == 11:
+    print("Novembro")
+
+elif numero == 12:
+    print("Dezembro")
---
+++ Ex. 22.py
@@ -0,0 +1,46 @@
+"""
+-----------------------------------------
+	Função:
+	Autor: Nicolas Florencio Alves.
+	Data de criação: 10/07/2021.
+	Data de modificação: 10/07/2021.
+-------------------------------------------
+"""
+
+numero = int(input("Informe o respectivo número do mês desejado: "))
+
+if numero == 1:
+    print("Janeiro - 31 dias")
+
+elif numero == 2:
+    print("Fevereiro - 30 dias")
+
+elif numero == 3:
+    print("Março - 31 dias")
+
+elif numero == 4:
+    print("Abril - 30 dias")
+
+elif numero == 5:
+    print("Maio - 31 dias")
+
+elif numero == 6:
+    print("Junho - 30 dias")
+
+elif numero == 7:
+    print("Julho - 31 dias")
+
+elif numero == 8:
+    print("Agosto - 31 dias")
+
+elif numero == 9:
+    print("Setembro - 30 dias")
+
+elif numero == 10:
+    print("Outubro - 31 dias")
+
+elif numero == 11:
+    print("Novembro - 30 dias")
+
+elif numero == 12:
+    print("Dezembro - 31 dias")
